9dd2707850091c28671f6ba491361d0b9218066a
Alexandra <105469117+mlle-aadf@users.noreply.github.com>
2025-02-20 00:03:53 -0500

    date test

--- client/src/contexts/EventsContext.js
+++ client/src/contexts/EventsContext.js
@@ -5,17 +5,17 @@ export const EventsContext = createContext();
 const EventsContextProvider = ({ children }) => {
   const monthsKeys = [
     { str: "JANUARY", date: "2024-01"},
-    { str: "FEBRUARY", date: "2024-02"},
-    { str: "MARCH", date: "2024-03"},
-    { str: "APRIL", date: "2024-04"},
-    { str: "MAY", date: "2024-05"},
-    { str: "JUNE", date: "2024-06"},
-    { str: "JULY", date: "2024-07"},
-    { str: "AUGUST", date: "2024-08"},
-    { str: "SEPTEMBER", date: "2024-09"},
-    { str: "OCTOBER", date: "2024-10"},
-    { str: "NOVEMBER", date: "2024-11"},
-    { str: "DECEMBER", date: "2024-12"},
+    { str: "FEBRUARY", date: "2025-02"},
+    { str: "MARCH", date: "2025-03"},
+    { str: "APRIL", date: "2025-04"},
+    { str: "MAY", date: "2025-05"},
+    { str: "JUNE", date: "2025-06"},
+    { str: "JULY", date: "2025-07"},
+    { str: "AUGUST", date: "2025-08"},
+    { str: "SEPTEMBER", date: "2025-09"},
+    { str: "OCTOBER", date: "2025-10"},
+    { str: "NOVEMBER", date: "2025-11"},
+    { str: "DECEMBER", date: "2025-12"},
   ];
 
   const [event, setEvent] = useState({})
